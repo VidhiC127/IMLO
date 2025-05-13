7e47dc369df21e6018c4f5881fec7468508c1090
VidhiC127 <hrg529@york.ac.uk>
2025-05-13 22:32:24 +0100

    Update imlo_cw_works (1).py
    - Added 3rd conv layer and 3rd fc layer.
    
    Epochs: 50
    Time: 1 hour 57 mins
    Accuracy: 76.29%

--- imlo_cw_works (1).py
+++ imlo_cw_works (1).py
@@ -29,7 +29,7 @@ train_set = torchvision.datasets.CIFAR10("Data", download-True, train=True, tran
 
 mean = train_set.data.mean(axis=(0,1,2)) / 255
 std = train_set.data.std(axis=(0,1,2)) / 255
-print(mean, std)
+print(f"Mean = {mean} Std = {std}")
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -42,9 +42,9 @@ train_loader = torch.utils.data.DataLoader(train_set, batch_size=Batch_Size, shu
 test_set = torchvision.datasets.CIFAR10("Data", download=True, train=False, transform=transform) # was test_tr
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=Batch_Size, shuffle=False)
 
-print(train_set.data.shape)
-print(test_set.data.shape)
-print(train_set.classes)
+print(f"Training set shape: {train_set.data.shape}")
+print(f"Testing set shape: {test_set.data.shape}")
+print(f"Classes: {train_set.classes}")
 
 class CIFAR10_nn(nn.Module):
   def __init__(self):
@@ -53,13 +53,15 @@ class CIFAR10_nn(nn.Module):
     #3 input channels, 32 output feature maps, 3x3 filter size, padding keeps image dimensions the same
     self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
     self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+    self.conv3 = nn.Conv2d(64, 128, 3, padding=1) #New conv layer
 
     #pooling layers reduce image size, takes max value in each 2x2 region
     self.pool = nn.MaxPool2d(2, 2)
 
     #fully connected layers connect every input to every output
-    self.fc1 = nn.Linear(64 * 8 * 8, 512)
-    self.fc2 = nn.Linear(512, 10)
+    self.fc1 = nn.Linear(128 * 4 * 4, 512) #adjusted input size
+    self.fc2 = nn.Linear(512, 256)
+    self.fc3 = nn.Linear(256, 10)
 
     #Dropout randomly turns off 25% of neurons during training
     #--> Prevents nn from memorising the training data
@@ -67,16 +69,28 @@ class CIFAR10_nn(nn.Module):
 
   #Defines how data flows through the network
   def forward(self, x):
+    
     #1. 1st conv layer -> reLU activation -> pooling
-    x = self.pool(F.relu(self.conv1(x)))
+    x = self.pool(F.relu(self.conv1(x))) #Img size: 32x32 -> 16x16
+
     #2. 2nd conv layer -> reLU activation -> pooling
-    x = self.pool(F.relu(self.conv2(x)))
-    #3. Flatten 3D feature maps into a 1D vector
-    x = x.view(-1, 64 * 8 * 8)
-    #4. 1st fully connected layer -> ReLU -> dropout
+    x = self.pool(F.relu(self.conv2(x))) #Img size: 16x16 -> 8x8
+
+    #3. 3rd conv layer -> reLU activation -> pooling 
+    x = self.pool(F.relu(self.conv3(x))) #Img size: 8x8 -> 4x4
+
+    #4. Flatten 3D feature maps into a 1D vector
+    x = x.view(-1, 128 * 4 * 4) #Adjusted values for new size
+
+    #5. 1st fully connected layer -> ReLU -> dropout
     x = self.dropout(F.relu(self.fc1(x)))
-    #5. Final outout layer
-    x = self.fc2(x)
+
+    #6. 2nd fully connected layer -> reLU -> dropout
+    x = self.dropout(F.relu(self.fc2(x)))
+
+    #7. Final outout layer
+    x = self.fc3(x)
+      
     return x
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -127,6 +141,7 @@ for epoch in range(EPOCHS):
       total += labels.size(0)
   
   accuracy = 100 * correct / total  # Should now be <= 100%
+  accuracies.append(accuracy)
   print(f'Epoch {epoch+1}/{EPOCHS} - Loss: {epoch_loss:.4f}, Accuracy: {accuracy:.2f}%')
 
 
